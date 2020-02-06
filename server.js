6fa47dd57c79556d9793286f742845891a2f3b01
chriscrichter <chris_richter@outlook.com>
2020-02-05 22:26:09 -0500

    Updated js file.

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const app =  express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//PORT
-const PORT = process.env.PORT || 8000;
+// Port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 
-//MIDDLEWARE
-
-/*  GET REQUEST */
+// Get request
 app.get('/api/notes', (req, res) => {
   fs.readFile('db/db.json', 'utf8', function(err, contents) {
     var words = JSON.parse(contents);
@@ -24,24 +22,20 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-/*  POST REQUEST */
+// Post request
 app.post('/api/notes', (req, res) => {
   fs.readFile('db/db.json',(err, data) => {
-    // Check for error
     if (err) throw err;
-    // Handle data gathering for json update
     let json = JSON.parse(data);
     let note = {
       title: req.body.title,
       text: req.body.text,
       id: uuidv1()
     }
-    // Add data to existing json array
+
     json.push(note);
 
-    // Write updated json to array 
     fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
-      // Check for error
       if (err) throw err;
       res.send('200');
     });
@@ -49,14 +43,12 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-/* DELETE REQUEST */
+// Delete request
 app.delete('/api/notes/:id', (req, res) => {
 
   fs.readFile('db/db.json',(err, data) => {
-    // Check for error
     if (err) throw err;
     let deleteId = req.params.id;
-    // Handle data gathering for json update
     let json = JSON.parse(data);
     json.forEach((item, i) =>{
       if (item.id.includes(deleteId)){ 
@@ -64,9 +56,7 @@ app.delete('/api/notes/:id', (req, res) => {
       }
     });
 
-    // Write updated json to array 
     fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
-      // Check for error
       if (err) throw err;
       res.send('200');
     });
@@ -74,9 +64,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 })
 
-
-/*************    ROUTES    ************/ 
-// Add two routes for the two pages
+// Routes 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -85,7 +73,5 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-
-// Set static folder to rtrieve css and js files
 app.use(express.static(path.join(__dirname, 'public')));
 
